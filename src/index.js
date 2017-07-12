5424f60e7510c8ef13e56ea3ceda00187323cd2e
Ben Whittle <benwhittle31@gmail.com>
2017-07-11 21:21:19 -0700

    added sass support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import './stylesheets/index.scss';
+import './styles/css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
