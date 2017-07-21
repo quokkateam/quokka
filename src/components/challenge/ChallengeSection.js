97d0c5cc5a63ae6c07413f3014f4cdccddb7104d
Andrew Noyes <anoyes34@gmail.com>
2017-07-21 13:54:36 -0700

    Add challenge card

--- src/components/challenge/ChallengeSection.js
+++ src/components/challenge/ChallengeSection.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 const ChallengeSection = () => {
   return (
-      <div className="container-fluid">
-        <div className="row">
-          <h2 className="challenge-section-title">Challenge</h2>
-          <p className="challenge-section-desc">Lorem Ipsum</p>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="challenge-card">
+          <h2 className="challenge-section-title challenge-card-title">Challenge</h2>
+          <div className="challenge-card-title-border center-block"></div>
+          <p className="challenge-card-body">
+            Take the first day of this week to check out the Harvard School
+            of Public Health's <a href="/challenge">Healthy Eating Plate
+            Guide</a> on how to make better food decisions. Pick what will be
+            an improvement for your health and then stick to it for every day
+            this week.
+          </p>
         </div>
       </div>
+    </div>
   )
 }
 
