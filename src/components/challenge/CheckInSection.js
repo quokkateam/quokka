989ea9f08e4912fd39c57a89a9a880fd05e137f1
Todd Warszawski <twarsz1414@gmail.com>
2017-07-23 20:59:17 -0700

    Making the challenge page look like the static image.

--- src/components/challenge/CheckInSection.js
+++ src/components/challenge/CheckInSection.js
@@ -5,7 +5,12 @@ const CheckInSection= () => {
       <div className="container-fluid">
         <div className="row">
           <h2 className="challenge-section-title">Check-In</h2>
-          <p className="challenge-section-desc">Lorem Ipsum</p>
+          <p className="challenge-section-desc">
+            Don't forget to submit your Check-in this week by DATE!
+            Check-ins help you earn Quokka points, provide us with feedback,
+            and make you eligible for this week's prizes!
+            <a href="/challenge"> Take me to my Week 4 Check-in</a>.
+          </p>
         </div>
       </div>
   )
