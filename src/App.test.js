06fbf218ef2519168a01d591b124d7f0e6d0ee87
Andrew Noyes <anoyes34@gmail.com>
2017-07-23 17:36:57 -0700

    Render /challenge and /check-in

--- src/App.test.js
+++ src/App.test.js
@@ -5,12 +5,31 @@ import { MemoryRouter } from 'react-router-dom';
 
 describe('across entire suite', function () {
 
-    it('renders without crashing', () => {
+    it('/ renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(
-        <MemoryRouter><App/></MemoryRouter>
+        <MemoryRouter initialEntries={[ '/' ]}>
+            <App />
+        </MemoryRouter>
         , div);
     });
 
+    it('/challenge renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+        <MemoryRouter initialEntries={[ '/challenge' ]}>
+            <App />
+        </MemoryRouter>
+        , div);
+    });
+
+    it('/check-in renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+        <MemoryRouter initialEntries={[ '/check-in' ]}>
+            <App />
+        </MemoryRouter>
+        , div);
+    });
 })
 
