4663fe30fa170bc7da345e008141cc6e82ae9f64
Cindy S <30301274+cshih7@users.noreply.github.com>
2017-08-01 14:22:58 -0700

    Update CheckInSection.js

--- src/components/challenge/CheckInSection.js
+++ src/components/challenge/CheckInSection.js
@@ -7,7 +7,7 @@ class CheckInSection extends Component {
         <div className="row">
           <div className="challenge-section-title">Check-In</div>
           <div className="challenge-section-desc">
-            Don't forget to submit your Check-in this week by Sunday, {this.props.endDate}!
+            Don't forget to submit your check-in this week by Sunday, {this.props.endDate}!
             Check-ins help you earn Quokka points, provide us with feedback,
             and make you eligible for this week's prizes!&nbsp;
             <a href="/check-in">Take me to my Week {this.props.weekNum} Check-In.</a>
