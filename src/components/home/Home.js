3ae5bdebd30515b07d583a35a6d6246a94dab6e8
Ben Whittle <benwhittle31@gmail.com>
2017-08-09 02:29:59 -0400

    character change per Cindy's request

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -30,7 +30,7 @@ class Home extends Component {
 
   onSignUp(email) {
     this.banner.update({
-      message: 'Thanks for joining the Quokka challenge! Check your email at **' + email + '** to verify your account and get the latest updates about the first challenge.',
+      message: 'Thanks for joining the Quokka Challenge! Check your email at **' + email + '** to verify your account and get the latest updates about the first challenge.',
       buttonText: 'Got it'
     });
   }
