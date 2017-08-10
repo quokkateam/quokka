9395c5fe881eb508f7b7c8b3b40a65cee12ef32c
Ben Whittle <benwhittle31@gmail.com>
2017-08-10 16:30:42 -0400

    Making LandingHeader have Challenges link instead of Sign In if user is authed

--- src/components/shared/headers/LandingHeader.js
+++ src/components/shared/headers/LandingHeader.js
@@ -1,6 +1,52 @@
 import React, { Component } from 'react';
+import Session from '../../../utils/Session';
 
 class LandingHeader extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.getLinks = this.getLinks.bind(this);
+  }
+  
+  getLinks() {
+    var links;
+    
+    if (Session.authed()) {
+      links = [
+        {
+          text: 'FAQ',
+          href: '/faq'
+        },
+        {
+          text: 'Challenges',
+          href: '/challenges'
+        }
+      ];
+    } else {
+      links = [
+        {
+          text: 'Sign In',
+          href: '/signin',
+          featured: true
+        },
+        {
+          text: 'FAQ',
+          href: '/faq'
+        }
+      ];
+    }
+    
+    return links.map((data, i) => {
+      var classes = ['header-nav-link'];
+      
+      if (data.featured) {
+        classes.push('featured');
+      }
+      
+      return <a key={i} href={data.href} className={classes.join(' ')}>{data.text}</a>;
+    });
+  }
+  
   render() {
     return (
       <header id="landingHeader">
@@ -11,10 +57,7 @@ class LandingHeader extends Component {
             </a>
           </div>
           <div className="header-right">
-            <div className="header-dktp">
-              <a href="/signin" className="header-nav-link featured">Sign In</a>
-              <a href="/faq" className="header-nav-link">FAQ</a>
-            </div>
+            <div className="header-dktp">{this.getLinks()}</div>
             <div className="header-mbl" onClick={this.props.onMenuClick}>
               <i className="fa fa-bars"></i>
             </div>
