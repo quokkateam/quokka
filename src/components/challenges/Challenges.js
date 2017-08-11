d7b459a88cffad3a81cf184e880722b1e21f98f9
Ben Whittle <benwhittle31@gmail.com>
2017-08-10 21:01:18 -0400

    add Challenges component and route to it

---
+++ src/components/challenges/Challenges.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Challenges extends Component {
+  render() {
+    return (
+      <div id="challenges">Challenges</div>
+    );
+  }
+}
+
+export default Challenges;
