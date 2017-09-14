c13239bcea5f27b82fc16d75d93215dbb53a5a9d
Ben Whittle <benwhittle31@gmail.com>
2017-09-14 15:48:33 -0700

    hooked up Challenges page and weekly prog bar to BE

--- src/components/challenges/Challenges.js
+++ src/components/challenges/Challenges.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
+import Ajax from '../../utils/Ajax';
 import WeeklyProgBar from '../widgets/WeeklyProgBar';
 
 class Challenges extends Component {
   
   constructor(props) {
     super(props);
+
+    this.setProgBarRef = this.setProgBarRef.bind(this);
 
     // from server
     this.data = {
@@ -19,14 +23,29 @@ class Challenges extends Component {
         'Group-Selected Challenge',
         'Positivity & Mindfulness'
       ]
-    }
+    };
+  }
+
+  setProgBarRef(ref) {
+    this.progBar = ref;
+  }
+
+  componentDidMount() {
+    Ajax.get('/api/challenges')
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.progBar.setState({
+          challenges: data.challenges,
+          weekNum: data.weekNum
+        });
+      });
   }
   
   render() {
     return (
       <div id="challenges">
         <div className="prog-bar-wrapper">
-          <WeeklyProgBar currentWeekIndex={this.data.weekNum - 1} weeks={this.data.weeks} />
+          <WeeklyProgBar ref={this.setProgBarRef}/>
         </div>
       </div>
     );
