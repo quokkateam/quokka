18bf391db940b0e60fbe56ffa28652ef1969b5eb
Ben Whittle <benwhittle31@gmail.com>
2017-10-08 15:23:22 -0700

    text change

--- src/components/admin/PrizeWinners.js
+++ src/components/admin/PrizeWinners.js
@@ -59,7 +59,7 @@ class PrizeWinners extends Component {
       <div id="prizeWinners">
         <div className="prize-winners-intro">
           <div className="prize-winners-title">Prize Winners</div>
-          <div className="prize-winners-subtitle">Select winners for the weekly challenges.</div>
+          <div className="prize-winners-subtitle">Select prize winners for each weekly challenge.</div>
         </div>
         <ul className="prize-winners-list">{this.populatePrizeWinnersList()}</ul>
       </div>
