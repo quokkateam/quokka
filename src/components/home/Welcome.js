0f8f533fcff5059f71052336c9acb928c3bbe95e
C S <Cindy@DN51s4on.SUNet>
2017-10-20 14:48:24 -0700

    Changed welcome text

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -7,7 +7,7 @@ class Welcome extends Component {
       <section id="welcome">
         <div className="dimmer"></div>
         <div className="welcome-content">
-          <h1 className="welcome-main-text">Welcome to Quokka</h1>
+          <h1 className="welcome-main-text">Hey Cindy!</h1>
           <p className="welcome-sub-text">Healthy habits for a happier campus</p>
           <SignUp onSubmitSchool={this.props.onSubmitSchool} onSignUp={this.props.onSignUp} />
         </div>
