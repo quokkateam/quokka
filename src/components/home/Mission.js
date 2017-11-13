f60985a25cf3f0363b90ffa95014ab2b06d1600c
Cindy S <30301274+cshih7@users.noreply.github.com>
2017-11-13 13:31:04 -0800

    Removing specific number of habits ("8 habits")
    Opening up number of habits we include

--- src/components/home/Mission.js
+++ src/components/home/Mission.js
@@ -7,7 +7,7 @@ class Mission extends Component {
         <div className="container-fluid">
           <div className="row">
             <h2 className="about-category-title">What's the Scoop?</h2>
-            <p className="about-category-desc">The Quokka Challenge is a collection of 8 habits that research shows can help make you happier and healthier. This includes getting better sleep, eating well, and being mindful. The challenge is simple: try each habit for one week. Chances are, you’ll feel better along the way.</p>
+            <p className="about-category-desc">The Quokka Challenge is a collection of habits that research shows can help make you happier and healthier. This includes getting better sleep, eating well, and being mindful. The challenge is simple: try each habit for one week. Chances are, you’ll feel better along the way.</p>
           </div>
         </div>
       </section>
