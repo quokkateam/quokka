e759b31d69d994adbe8fd4c9795c7c8d55f64823
Cindy S <30301274+cshih7@users.noreply.github.com>
2018-03-18 14:57:19 -0700

    Add questions to FAQ page

--- src/components/faq/FAQ.js
+++ src/components/faq/FAQ.js
@@ -13,15 +13,15 @@ class FAQ extends Component {
       faqs: [
         {
           question: 'What\'s a Quokka?',
-          answer: 'Quokkas (pronunciation: kwok-uh) are small marsupials found in southwestern Australia that are often described as the "world\'s happiest animal." About the size of a teddy bear or house cat, quokkas are known for their friendliness and happy demeanor. We hope to inspire college students to start their Quokka journey and get happy with the Quokka Challenge!'
+          answer: 'Quokkas (pronunciation: kwok-uh) are small marsupials found in southwestern Australia that are often described as the "world\'s happiest animal." Quokkas are known for their friendliness and happy demeanor, and we hope to inspire users to start their Quokka journey and get happy with the Quokka Challenge!'
         },
         {
           question: 'What\'s the Quokka Challenge?',
-          answer: 'The Quokka Challenge promotes healthy behaviors that have been empirically shown to improve well-being (e.g., exercise, sleep, healthy eating, mindfulness). The 8-week program is coordinated across multiple universities each year, and students can sign up if their school is part of the current cohort.'
+          answer: 'The Quokka Challenge promotes healthy behaviors that have been empirically shown to improve well-being (e.g., exercise, sleep, healthy eating, mindfulness). The program is coordinated across multiple universities and communities each year, and students can sign up if their school is part of the current cohort.'
         },
         {
           question: 'What are the weekly challenges like?',
-          answer: 'When you sign up for the program, you’re given weekly challenges, each based around one healthy habit. These challenges are backed with research, provide information about campus resources, and connect you to other challengers through local events. When you submit your check-in at the end of the week, you can even win prizes for trying these habits out!'
+          answer: 'When you sign up for the program, youâ€™re given weekly challenges, each based around one healthy habit. These challenges are backed with research, provide information about campus resources, and connect you to other challengers through local events. When you submit your check-in at the end of the week, you can even win prizes for trying these habits out!'
         },
         {
           question: 'What if there are multiple challenge options in a week?',
@@ -29,15 +29,19 @@ class FAQ extends Component {
         },
         {
           question: 'What kinds of prizes can we win?',
-          answer: 'Depending on your campus, prizes can range from school-sponsored gear and gift cards to massages and private fitness classes. You’ll get more information about all the prizes in each week’s email. If you submit a check-in for all 8 weeks, you might receive a bonus Challenge Champion prize.'
+          answer: 'Depending on your campus, prizes can range from school-sponsored gear and gift cards to massages and private fitness classes. You\'ll get more information about all the prizes in each week\'s email. If you submit a check-in for all weeks, you might receive a bonus Challenge Champion prize.'
         },
         {
-          question: 'Which schools are participating this fall?',
+          question: 'Which schools are participating right now?',
           answer: 'Check out our home page to sign up and see which other schools are hosting the Quokka Challenge.'
         },
         {
           question: 'I want my school to join the Quokka Challenge. Who can I contact for more information?',
-          answer: <QuokkaMarkdown source='Contact us at [team@quokkachallenge.com](mailto:team@quokkachallenge.com), or enter your info below.'/>
+          answer: <QuokkaMarkdown source='Contact us at [team@quokkachallenge.com](mailto:team@quokkachallenge.com), or enter your info at the bottom of the page.'/>
+        },
+        {
+          question: 'Want to join the Quokka development team?',
+          answer: <QuokkaMarkdown source='Email [team@quokkachallenge.com](mailto:team@quokkachallenge.com) to ask about opportunities!'/>
         }
       ]
     };
