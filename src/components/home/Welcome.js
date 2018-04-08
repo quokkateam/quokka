d8804044afbeb8f19a582db63d9f34f7fdde7a67
Cindy Shih <cshih@MacBook-Pro-2.local>
2018-04-08 15:14:08 -0700

    fixed syntax error in frontpage

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -9,9 +9,9 @@ class Welcome extends Component {
         <div className="welcome-content">
           <h1 className="welcome-main-text">Welcome to Quokka</h1>
           <p className="welcome-sub-text">Healthier habits for a happier campus</p>
-          <br>
+          <br />
           <a href='http://eepurl.com/dqEEqP'> Joining UCSB&#039;s Challenge? Click here to sign up! </a>
-          <br>
+          <br />
           <SignUp onSubmitSchool={this.props.onSubmitSchool} onSignUp={this.props.onSignUp} />
         </div>
       </section>
