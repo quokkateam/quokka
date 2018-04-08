6a2a56df669c16d57def83c635cc20ebc7a5f0a8
Cindy Shih <cshih@MacBook-Pro-2.local>
2018-04-08 14:41:34 -0700

    Added link to UCSB

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -9,6 +9,9 @@ class Welcome extends Component {
         <div className="welcome-content">
           <h1 className="welcome-main-text">Welcome to Quokka</h1>
           <p className="welcome-sub-text">Healthier habits for a happier campus</p>
+          <br>
+          <a href="http://eepurl.com/dqEEqP">Joining UCSB's Challenge? Click here to sign up!</a>
+          <br>
           <SignUp onSubmitSchool={this.props.onSubmitSchool} onSignUp={this.props.onSignUp} />
         </div>
       </section>
