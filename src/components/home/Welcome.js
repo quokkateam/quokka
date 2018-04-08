9febb99b6900b618cb4b9bab2c67d8cb6280a282
Cindy Shih <cshih@MacBook-Pro-3.local>
2018-04-08 15:27:11 -0700

    Deploy Assets

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -10,7 +10,9 @@ class Welcome extends Component {
           <h1 className="welcome-main-text">Welcome to Quokka</h1>
           <p className="welcome-sub-text">Healthier habits for a happier campus</p>
           <br />
-          <a href='http://eepurl.com/dqEEqP'> Joining UCSB&#039;s Challenge? Click here to sign up! </a>
+          <center>
+            <a href='http://eepurl.com/dqEEqP'> Joining UCSB&#039;s Challenge? Click here to sign up! </a>
+          </center>
           <br />
           <SignUp onSubmitSchool={this.props.onSubmitSchool} onSignUp={this.props.onSignUp} />
         </div>
